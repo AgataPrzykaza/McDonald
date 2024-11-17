9c22ceee17a5c9beda33094013e03b22af623287
AgataPrzykaza <agataprzykaza89@gmail.com>
2024-11-17 18:25:39 +0100

    preview location added

---
+++ McDonald/Views/Order Views/Elements/LocationCardView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCardView.swift
+//  McDonald
+//
+//  Created by Agata Przykaza on 17/11/2024.
+//
+
+import SwiftUI
+
+struct LocationCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationCardView()
+}
